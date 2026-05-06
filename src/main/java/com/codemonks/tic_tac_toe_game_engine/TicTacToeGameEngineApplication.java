1777b41eebee1c473495b0dfaed818bf896f4d04
Pankaj-Dhami007 <pankajdhami@neepanlok.com>
2026-05-06 11:57:01 +0530

    Initial tic tac toe engine

---
+++ src/main/java/com/codemonks/tic_tac_toe_game_engine/TicTacToeGameEngineApplication.java
@@ -0,0 +1,13 @@
+package com.codemonks.tic_tac_toe_game_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeGameEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeGameEngineApplication.class, args);
+	}
+
+}
