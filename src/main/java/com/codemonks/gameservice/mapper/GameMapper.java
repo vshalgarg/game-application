f170d038dee8b8f734fd69ae4c82053268b2e78e
PankajYadav <pankajyadav@codemonks.in>
2026-06-26 16:05:21 +0530

    GameMapperImproved

--- src/main/java/com/codemonks/gameservice/mapper/GameMapper.java
+++ src/main/java/com/codemonks/gameservice/mapper/GameMapper.java
@@ -3,7 +3,9 @@ package com.codemonks.gameservice.mapper;
 import com.codemonks.gameservice.engineModule.dto.request.EngineStartGameRequestDTO;
 import com.codemonks.gameservice.entity.PlayerEntity;
 import com.codemonks.gameservice.entity.RoomEntity;
+import com.codemonks.gameservice.enums.RoomPlayerRole;
 
+import java.util.Comparator;
 import java.util.List;
 
 public final class GameMapper {
@@ -12,7 +14,11 @@ public final class GameMapper {
             RoomEntity room,
             List<PlayerEntity> players
     ) {
-
+        players.sort(
+                Comparator.comparing(
+                        p -> p.getRole() == RoomPlayerRole.HOST ? 0 : 1
+                )
+        );
         List<Long> playerIds = players.stream()
                 .map(PlayerEntity::getUserId)
                 .toList();
