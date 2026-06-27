30164d746b14cb5c9382a2b61359aaafb897c5b0
PankajYadav <pankajyadav@codemonks.in>
2026-06-27 13:27:04 +0530

    engineurl

--- src/main/java/com/codemonks/tic_tac_toe_game_engine/constant/EngineApiUrlConstants.java
+++ src/main/java/com/codemonks/tic_tac_toe_game_engine/constant/EngineApiUrlConstants.java
@@ -2,7 +2,7 @@ package com.codemonks.tic_tac_toe_game_engine.constant;
 
 public class EngineApiUrlConstants {
 
-    public static final String BASE_API ="/api/v1/engine";
+    public static final String BASE_API ="/engine";
     public static final String START_GAME="/start-game";
     public static final String MAKE_MOVE="/make-move";
 
