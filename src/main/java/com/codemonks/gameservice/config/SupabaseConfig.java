38b7af86d20455ec20140d7395a9e5b3f1ef9d48
Vishal Garg Admin <vishalgarg@neepanlok.com>
2026-06-30 17:28:10 +0530

    logged

--- src/main/java/com/codemonks/gameservice/config/SupabaseConfig.java
+++ src/main/java/com/codemonks/gameservice/config/SupabaseConfig.java
@@ -1,19 +1,21 @@
 package com.codemonks.gameservice.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class SupabaseConfig {
 
     private final SupabaseProperties properties;
 
     @Bean
     public WebClient supabaseWebClient() {
-
+      log.info("supabase api key: {}", properties.getApiKey());
         return WebClient.builder()
                 .baseUrl(properties.getUrl())
                 .defaultHeader(
