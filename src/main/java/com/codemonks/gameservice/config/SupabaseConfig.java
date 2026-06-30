f12219dee5989ab63170369e5eda127c5504ee4e
Vishal Garg Admin <vishalgarg@neepanlok.com>
2026-06-30 17:48:46 +0530

    corrected properties

--- src/main/java/com/codemonks/gameservice/config/SupabaseConfig.java
+++ src/main/java/com/codemonks/gameservice/config/SupabaseConfig.java
@@ -15,7 +15,6 @@ public class SupabaseConfig {
 
     @Bean
     public WebClient supabaseWebClient() {
-      log.info("supabase url: {} api key: {}", properties.getUrl(), properties.getApiKey());
         return WebClient.builder()
                 .baseUrl(properties.getUrl())
                 .defaultHeader(
