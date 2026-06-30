41c4a8a4a0bb2e5db9b73496ef5829b9956e8d94
Vishal Garg <vishalgarg053@gmail.com>
2026-06-30 17:48:10 +0530

    Update EngineApiUrlConstants.java

--- src/main/java/com/codemonks/tic_tac_toe_game_engine/constant/EngineApiUrlConstants.java
+++ src/main/java/com/codemonks/tic_tac_toe_game_engine/constant/EngineApiUrlConstants.java
@@ -2,7 +2,7 @@ package com.codemonks.tic_tac_toe_game_engine.constant;
 
 public class EngineApiUrlConstants {
 
-    public static final String BASE_API ="/engine";
+    public static final String BASE_API ="/game-engine";
     public static final String START_GAME="/api/v1/start-game";
     public static final String MAKE_MOVE="/api/v1/make-move";
 
